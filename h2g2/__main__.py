bcec4071ee3bd91659bb00dd2804bd9384f81de6
Pierre Marijon <pierre@marijon.fr>
2019-11-11 16:26:18 +0100

    Module split merge overlapping variant into one

--- h2g2/__main__.py
+++ h2g2/__main__.py
@@ -5,10 +5,6 @@ import csv
 import sys
 import argparse
 
-# package import
-from .split import split
-from .prepare import prepare
-
 def main(args: None):
     if args is None:
         args = sys.argv[1:]
@@ -17,17 +13,21 @@ def main(args: None):
     subparsers = parser.add_subparsers(dest='subparser_name', title='subcommands', help='additional help')
     
     subparsers_prepare = subparsers.add_parser("prepare", help='Prepare pipeline run of h2g2')
-    subparsers_prepare.add_argument("-a", "--assembly", nargs="*", help="list of assembly you want use")
+    subparsers_prepare.add_argument("-a", "--assembly", nargs="*", required=True, help="list of assembly you want use")
     
     subparsers_split_variant = subparsers.add_parser("split", help='Extract complicate region from vcf')
-    subparsers_split_variant.add_argument("-v", "--vcf-file", required=True, help="Path to vcf")
-    subparsers_split_variant.add_argument("-o", "--output", required=True, help="Path where output was write")
+    subparsers_split_variant.add_argument("-v", "--vcf-files", nargs="*", required=True, help="Path to vcf")
+    subparsers_split_variant.add_argument("-r", "--references", required=True, help="Path to references")
     
+    subparsers_split_variant.add_argument("-o", "--output", required=True, help="Path where output was write")
     args = parser.parse_args()
 
+    
     if args.subparser_name == "prepare":
+        from .prepare import prepare # import only when it's required
         return prepare(args)
     elif args.subparser_name == "split":
+        from .split import split # import only when it's required
         return split(args)
     else:
         print("You need to use a subcommands", file=sys.stderr)
--- h2g2/split/__init__.py
+++ h2g2/split/__init__.py
@@ -1,3 +1,123 @@
+#!/usr/bin/env python
+
+# standard import
+import sys
+from collections import defaultdict
+
+# pip import
+import pysam
+import vcf
+from Bio import SeqIO
 
 def split(args):
-    print(args)
+    
+    tig2seq = read_ref(args.references)
+
+    variants, vcf2haploid, haploid2vcf = read_vcfs(args.vcf_files)
+    
+    blocks = generate_block(variants)
+            
+    print("\t".join(["#CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT", *args.vcf_files]))
+
+    for block in blocks:
+        poss = list()
+        for v in block:
+            poss.append(v[0].start)
+            poss.append(v[0].end)
+        poss.sort()
+        
+        begin = poss[0] + 1
+        end = poss[-1] + 1
+        chrom = block[0][0].CHROM
+        ref = str(tig2seq[chrom][begin-1:end-1])
+
+        #print("begin", begin)
+        #print("end", end)
+        
+        alts = [""] * len(args.vcf_files)
+        hetero = [""] * len(args.vcf_files)
+        for v in block:
+            variant = v[0]
+            
+            #print("variant.begin", variant.start + 1)
+            #print("variant.end", variant.end + 1)            
+
+            change_pos_begin = dist(begin, (variant.start + 1))
+            change_pos_end = dist(len(ref), (end - (variant.end + 1)))
+            #print("ref", ref)
+            alt = str(variant.ALT[0])
+            #print("change_pos_begin", change_pos_begin)
+            #print("ref[:change_pos]", ref[:change_pos_begin])
+            #print("change_pos_end", change_pos_end)
+            #print("ref[change_pos:]", ref[change_pos_end:])
+            #print("alt", alt)
+            alt = ref[:change_pos_begin]+alt+ref[change_pos_end:]
+            #print("alt", alt)
+            alts[vcf2haploid[v[1]] - 1] = alt
+            #print("alts", alts)
+            hetero[vcf2haploid[v[1]] - 1] = variant.genotype('sample')['GT'].replace("1", str(vcf2haploid[v[1]]))
+            
+        print("\t".join([chrom, # #CHROM
+                        str(begin), # POS
+                        '.',   # ID
+                        ref,   # REF
+                        ','.join(alts), # ALT
+                        '.',   # QUAL
+                        '.',   # FILTER
+                        '.',   # INFO
+                        '.',   # FORMAT
+                        '\t'.join(hetero) 
+                        ])
+        )
+
+def dist(a, b):
+    if a > b:
+        return a - b
+    else:
+        return b - a 
+
+        
+def read_ref(path):
+    tig2seq = dict()
+    with open(path, "rU") as handle:
+        for record in SeqIO.parse(handle, "fasta"):
+            tig2seq[record.id] = record.seq
+
+    return tig2seq
+
+def read_vcfs(paths):
+    variants = list()
+    vcf2haploid = dict()
+    haploid2vcf = dict()
+    haplo_id = 0
+    for vcf_file in paths:
+        haplo_id += 1
+        vcf_reader = vcf.Reader(open(vcf_file))
+
+        vcf2haploid[vcf_file] = haplo_id
+        haploid2vcf[haplo_id] = vcf_file
+        for record in vcf_reader:
+            variants.append((record, vcf_file))
+
+    return variants, vcf2haploid, haploid2vcf
+
+def generate_block(variants):
+    block = list()
+    list_block = list()
+    for variant in sorted(variants, key=lambda x: x[0].start):
+        if len(block) == 0:
+            block.append(variant)
+            continue
+        
+        if block[-1][0].CHROM == variant[0].CHROM and block[-1][0].end > variant[0].start:
+            block.append(variant)
+        else:
+            if len(block) > 1:
+                list_block.append(block)
+            block = list()
+            block.append(variant)
+
+    if len(block) > 1:
+        list_block.append(block)
+
+    return list_block
